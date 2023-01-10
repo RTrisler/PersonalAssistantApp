8a36f9f16ea2873b6ac746e3c8c0e5b9e1397fdc
Reid Trisler <reidtrisler@yahoo.com>
2023-01-10 16:27:40 -0600

    Created Home Page along with App navigation via drawer
    Implemented a React Navigation Drawer with gesture support that allows users to navigate through the app.

--- mello/mello/App.js
+++ mello/mello/App.js
@@ -1,20 +1,30 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './Home'
+import Settings from './Settings';
+import TimeManagement from './TimeManagement';
+
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Mello</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Drawer.Navigator initialRouteName="Home" screenOptions={{
+                                                drawerStyle: {
+                                                  backgroundColor: '#839496'
+                                                }
+    }}>
+      <Drawer.Screen name="Mello" component={Home} options={{ title: 'Welcome', headerStyle: {
+              backgroundColor: '#2aa198'
+           } }} />
+      <Drawer.Screen name="Settings" component={Settings} />
+      <Drawer.Screen name="TimeManagement" component={TimeManagement} options={{ headerShown: false}} />
+    </Drawer.Navigator>
+  </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ mello/mello/TimeManagement.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler'
+
+export default function TimeManagement() {
+  return (
+    <View style={styles.container}>
+      <Text>TimeManagement!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
