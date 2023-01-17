7cda816a343fe335a5c28c3c23fcface4c21b315
J-Vieux <94016719+J-Vieux@users.noreply.github.com>
2023-01-17 17:43:41 -0600

    weekly and daily calendar view, ver1

--- mello/mello/TimeManagement.js
+++ mello/mello/TimeManagement.js
@@ -1,92 +1,33 @@
-import react from 'react';
-//import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity, StatusBar } from 'react-native';
-import {agenda} from 'react-native-calendars';
-import {card} from 'react-native-paper';
-import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler'
-
-/*
-export default function TimeManagement() {
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import WeeklyCalendar from 'react-native-weekly-calendar';
+ 
+export default function App() {
+  const sampleEvents = [
+    { 'start': '2023-01-13 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
+    { 'start': '2023-01-14 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
+    { 'start': '2023-01-15 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
+    { 'start': '2023-01-15 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
+    { 'start': '2023-01-15 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
+    { 'start': '2023-01-16 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
+    { 'start': '2023-01-16 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
+    { 'start': '2023-01-16 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
+    { 'start': '2023-01-16 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
+    { 'start': '2023-01-16 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
+  ]
+ 
   return (
     <View style={styles.container}>
-      <Text>TimeManagement!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}*/
-
-const timeToString = (time) => {
-  const date = new Date(time);
-  return date.toISOString().split('T')[0];
-}
-
-const App = () => {
-  const [items, setItems] = React.useState({});
-  
-  const loadItems = (day) => {
-    setTimeout(() => {
-      for (let i = -15; i<85; i++){
-        const time = day.timestamp + i * 24 * 60 * 60 * 1000;
-        const strTime = timeToString(time);
-
-        if (!items[strTime]){
-          items[strTime] = [];
-
-          const numItems = Math.floor(Math.random()*3+1);
-          for (let j = 0; j < numItems; j++){
-            items[strTime].push({
-              name: 'item for ' + strTime + ' #' + j,
-              height: Math.max(10, Math.floor(Math.random()*150)),
-              day: strTime
-            });
-          }
-        }
-      }
-      const newItem = {};
-      Object.keys(items).forEach(key => {
-        newItems[key] = items[key];
-      });
-      setItems(newItems);
-    }, 1000);
-  }
-  const renderItem = (item) => {
-    return(
-      <TouchableOpacity style={styles.item}>
-        <Card>
-          <Card.Content>
-            <View>
-              <Text>{item.name}</Text>
-            </View>
-          </Card.Content>
-        </Card>
-      </TouchableOpacity>
-    );
-  }
-  return(
-    <View style={styles.container}>
-      <agenda
-        items={items}
-        loadItemsForMonth={loadItems}
-        selected={'2023-1-1'}
-        refreshControl={null}
-        showClosingKnob={true}
-        refreshing={false}
-        renderItem={renderItem}
-        />
+      <WeeklyCalendar events={sampleEvents} style={{ height: 400 }} />
     </View>
   );
 }
-
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    borderRadius: 5,
-    padding: 10,
-    marginRight:10,
-    marginTop:17,
-    backgroundColor: 'white',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
