e6efe2441d7b0475da4690975e47de50eedb68bb
1234thien <1234thien@gmail.com>
2023-01-19 14:37:23 -0600

    added ability to add events to event list

--- mello/mello/WeekView.js
+++ mello/mello/WeekView.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View, Text, Button, TextInput } from 'react-native';
 import WeeklyCalendar from 'react-native-weekly-calendar';
-import {Agenda, Calendar} from 'react-native-calendars';
- 
+import DateTimePicker from '@react-native-community/datetimepicker';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Modal from "react-native-modal";
 export default function App() {
   const sampleEvents = [
     { 'start': '2023-01-13 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
@@ -16,9 +17,100 @@ export default function App() {
     { 'start': '2023-01-16 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
     { 'start': '2023-01-16 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
   ]
- 
+
+  //event maker window visibility
+  const [isEventMakerVisible, setEventMakerVisible] = useState(false);
+  const toggleEventMaker = () => {
+    setEventMakerVisible(!isEventMakerVisible);
+  };
+
+
+  const [startDate, setStartDate] = useState(new Date());
+  const [startTime, setstartTime] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const [endTime, setEndTime] = useState(new Date());
+  const[itemText, setitemText] = useState('empty');
+
+  //functions for changing date/time vars
+  const onChangeStartDate = (event, selectedStartDate) => {
+    const currentStartDate = selectedStartDate;
+    setStartDate(currentStartDate);
+  };
+  const onChangeStartTime = (event, selectedstartTime) => {
+    const currentstartTime = selectedstartTime;
+    setstartTime(currentstartTime);
+  };
+  const onChangeEndDate = (event, selectedEndDate) => {
+    const currentEndDate = selectedEndDate;
+    setEndDate(currentEndDate);
+  };
+  const onChangeEndTime = (event, selectedEndTime) => {
+    const currentEndTime = selectedEndTime;
+    setEndTime(currentEndTime);
+  };
+
+  const addToEvents = () => {
+    const fStartDate = startDate.getFullYear() + '-' + (startDate.getMonth() + 1) + '-' + startDate.getDate();
+    const fStartTime = startTime.getHours() + ':' + startTime.getMinutes() + ':' + startTime.getSeconds();
+    sampleEvents.push({ 'start': fStartDate + ' ' + fStartTime, 'duration': '00:00:00', 'note': itemText })
+    console.log(sampleEvents[sampleEvents.length-1]);
+    toggleEventMaker();
+  };
+
   return (
     <View style={styles.container}>
+      <Button title="Show EventMaker" onPress={toggleEventMaker} />
+      <Modal isVisible={isEventMakerVisible}>
+        <View style={styles.eventMaker}>
+        <Text style={{fontWeight: 'bold', fontSize: 20}}> Item: </Text>
+        <TextInput 
+          style={styles.textInput}
+          onChangeText={setitemText}
+        ></TextInput>
+
+          <View style={styles.dateTimeField}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}> Date: </Text>
+              <DateTimePicker
+                testID='startDatePicker'
+                value={startDate}
+                mode={'date'}
+                onChange={onChangeStartDate}
+              />
+          </View>
+          <View style={styles.dateTimeField}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}> Time: </Text>
+              <DateTimePicker
+                testID='startTimePicker'
+                value={startTime}
+                mode={'time'}
+                onChange={onChangeStartTime}
+              />
+          </View>
+          {false &&(<View style={styles.dateTimeField}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}> End Date: </Text>
+              <DateTimePicker
+                testID='endDatePicker'
+                value={endDate}
+                mode={'date'}
+                onChange={onChangeEndDate}
+                minimumDate={startDate}
+              />
+          </View>)}
+          {false &&(<View style={styles.dateTimeField}>
+            <Text style={{fontWeight: 'bold', fontSize: 20}}> Start Time: </Text>
+            <DateTimePicker
+              testID='endTimePicker'
+              value={endTime}
+              mode={'time'}
+              onChange={onChangeEndTime}
+            />
+          </View>)}
+          <Button title="Add To Events" onPress={addToEvents} />
+          <Button title="Cancel" onPress={toggleEventMaker} />
+        </View>
+      </Modal>
+
+
       <WeeklyCalendar 
       events={sampleEvents} 
       style={{ height: 400 }}
@@ -32,8 +124,34 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ffffff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+    button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  eventMaker: {
+    padding: 20,
+    backgroundColor: '#dddddd',
+    borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textInput: {
+    height: 40,
+    width: 300,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  dateTimeField: {
+    width: 300,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'space-between'
   }
+
 });
