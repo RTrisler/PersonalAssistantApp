42236f8c7451e4092e7eef845c6c6dac4a47af18
1234thien <1234thien@gmail.com>
2023-01-19 14:55:03 -0600

    modified some test sample to see different configurations

--- mello/mello/WeekView.js
+++ mello/mello/WeekView.js
@@ -6,16 +6,16 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import Modal from "react-native-modal";
 export default function App() {
   const sampleEvents = [
-    { 'start': '2023-01-13 09:00:00', 'duration': '00:20:00', 'note': 'Walk my dog' },
-    { 'start': '2023-01-14 14:00:00', 'duration': '01:00:00', 'note': 'Doctor\'s appointment' },
-    { 'start': '2023-01-15 08:00:00', 'duration': '00:30:00', 'note': 'Morning exercise' },
+    { 'start': '2023-01-13 09:00:00', 'duration': '0:20:00', 'note': 'Walk my dog' },
+    { 'start': '2023-01-14 14:00:00', 'duration': '1:00:00', 'note': 'Doctor\'s appointment' },
+    { 'start': '2023-01-15 08:00:00', 'duration': '0:30:00', 'note': 'Morning exercise' },
     { 'start': '2023-01-15 14:00:00', 'duration': '02:00:00', 'note': 'Meeting with client' },
     { 'start': '2023-01-15 19:00:00', 'duration': '01:00:00', 'note': 'Dinner with family' },
-    { 'start': '2023-01-16 09:30:00', 'duration': '01:00:00', 'note': 'Schedule 1' },
-    { 'start': '2023-01-16 11:00:00', 'duration': '02:00:00', 'note': 'Schedule 2' },
-    { 'start': '2023-01-16 15:00:00', 'duration': '01:30:00', 'note': 'Schedule 3' },
-    { 'start': '2023-01-16 18:00:00', 'duration': '02:00:00', 'note': 'Schedule 4' },
-    { 'start': '2023-01-16 22:00:00', 'duration': '01:00:00', 'note': 'Schedule 5' }
+    { 'start': '2023-01-16 09:30:00', 'duration': '1:00:00', 'note': 'Schedule 1' },
+    { 'start': '2023-01-16 11:00:00', 'duration': '2:00:00', 'note': 'Schedule 2' },
+    { 'start': '2023-01-16 15:00:00', 'duration': '1:30:00', 'note': 'Schedule 3' },
+    { 'start': '2023-01-16 18:00:00', 'duration': '2:00:00', 'note': 'Schedule 4' },
+    { 'start': '2023-01-16 22:00:00', 'duration': '1:00:00', 'note': 'Schedule 57' }
   ]
 
   //event maker window visibility
@@ -59,7 +59,7 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Button title="Show EventMaker" onPress={toggleEventMaker} />
+      <Button title="Add Event" onPress={toggleEventMaker} />
       <Modal isVisible={isEventMakerVisible}>
         <View style={styles.eventMaker}>
         <Text style={{fontWeight: 'bold', fontSize: 20}}> Item: </Text>
