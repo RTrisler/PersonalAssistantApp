b88bcf357b486c90c56ace70ef8ea21da40f549f
Reid Trisler <reidtrisler@yahoo.com>
2023-02-07 16:47:45 -0600

    Adding animation changes

--- mello/mello/App.js
+++ mello/mello/App.js
@@ -1,39 +1,21 @@
 import 'react-native-gesture-handler';
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Ionicons } from 'react-native-vector-icons/Ionicons';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-import Home from './Home'
-import Settings from './Settings';
-import TimeManagement from './TimeManagement';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import AgendaView from './AgendaView';
+import SplashScreen from './SplashScreen';
 
-const TabNav = createBottomTabNavigator();
-function TheTabs(){
-  return(
-    <TabNav.Navigator screenOptions={{
-         tabBarActiveTintColor: '#467599',
-         tabBarInactiveTintColor: '#fcf7ff', 
-         tabBarActiveBackgroundColor: '#92828d',
-         tabBarInactiveBackgroundColor:'#022b3a',
-         headerShown: false
-         }}>
-      <TabNav.Screen name="Home" component={Home}/>
-      <TabNav.Screen name="Manage Time" component={AgendaView}/>
-      <TabNav.Screen name='Settings' component={Settings}/>
-    </TabNav.Navigator>
-  );
-}
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <TheTabs/>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <SplashScreen></SplashScreen>
+    </SafeAreaProvider>
   );
 }
 
--- mello/mello/Home.js
+++ mello/mello/Home.js
@@ -1,25 +1,48 @@
-import React from 'react'
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler'
+
+import AgendaView from './AgendaView';
+import Settings from './Settings';
+import TimeManagement from './TimeManagement';
+import Character from './Character';
+
 
 const BGColor = "#004052"
 
+const TabNav = createBottomTabNavigator();
+function TheTabs(){
+  return(
+    <TabNav.Navigator screenOptions={{
+         tabBarActiveTintColor: '#467599',
+         tabBarInactiveTintColor: '#fcf7ff', 
+         tabBarActiveBackgroundColor: '#92828d',
+         tabBarInactiveBackgroundColor:'#022b3a',
+         headerShown: false
+         }}>
+      <TabNav.Screen name='Home' component={Character}/>
+      <TabNav.Screen name="Manage Time" component={TimeManagement}/>
+      <TabNav.Screen name='Settings' component={Settings}/>
+    </TabNav.Navigator>
+  );
+}
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Mello Home</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <TheTabs />
+    </NavigationContainer>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: BGColor,
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
