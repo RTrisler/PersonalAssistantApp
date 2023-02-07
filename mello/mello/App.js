5a3192c6c223a0f28c7540add1349be29f8ef1cc
Reid Trisler <reidtrisler@yahoo.com>
2023-02-06 22:42:14 -0600

    Revert "Merge branch 'main' into PA-AL-5"
    This reverts commit c299452e33bb5cf5adb6aafd9f16e409388d0742, reversing
    changes made to faa06c1efa29770b55f58dd4d8e2b30b74581b3b.

--- mello/mello/App.js
+++ mello/mello/App.js
@@ -9,24 +9,7 @@ import { Ionicons } from 'react-native-vector-icons/Ionicons';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import SplashScreen from './SplashScreen';
-import AgendaView from './AgendaView';
 
-const TabNav = createBottomTabNavigator();
-function TheTabs(){
-  return(
-    <TabNav.Navigator screenOptions={{
-         tabBarActiveTintColor: '#467599',
-         tabBarInactiveTintColor: '#fcf7ff', 
-         tabBarActiveBackgroundColor: '#92828d',
-         tabBarInactiveBackgroundColor:'#022b3a',
-         headerShown: false
-         }}>
-      <TabNav.Screen name="Home" component={Home}/>
-      <TabNav.Screen name="Manage Time" component={AgendaView}/>
-      <TabNav.Screen name='Settings' component={Settings}/>
-    </TabNav.Navigator>
-  );
-}
 
 export default function App() {
   return (
