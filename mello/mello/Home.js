6a9d1cc546d56c15e55eb37c1b01b4ed45d3c3e2
Reid Trisler <reidtrisler@yahoo.com>
2023-02-07 14:24:51 -0600

    Revert "Merge pull request #11 from RTrisler/PA-AL-5"
    This reverts commit ce9c571ac02b019f81d4ba85193eb2359981c509, reversing
    changes made to 4fad8ea8ec8c01ae180328b239a13966469bf94d.

--- mello/mello/Home.js
+++ mello/mello/Home.js
@@ -1,48 +1,25 @@
-import React from 'react';
+import React from 'react'
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler'
-
-import AgendaView from './AgendaView';
-import Settings from './Settings';
-import TimeManagement from './TimeManagement';
-import Character from './Character';
-
 
 const BGColor = "#004052"
 
-const TabNav = createBottomTabNavigator();
-function TheTabs(){
-  return(
-    <TabNav.Navigator screenOptions={{
-         tabBarActiveTintColor: '#467599',
-         tabBarInactiveTintColor: '#fcf7ff', 
-         tabBarActiveBackgroundColor: '#92828d',
-         tabBarInactiveBackgroundColor:'#022b3a',
-         headerShown: false
-         }}>
-      <TabNav.Screen name='Home' component={Character}/>
-      <TabNav.Screen name="Manage Time" component={TimeManagement}/>
-      <TabNav.Screen name='Settings' component={Settings}/>
-    </TabNav.Navigator>
-  );
-}
 
 export default function Home() {
   return (
-    <NavigationContainer>
-      <TheTabs />
-    </NavigationContainer>
-    
+    <View style={styles.container}>
+      <Text>Mello Home</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'black',
+    backgroundColor: BGColor,
     alignItems: 'center',
     justifyContent: 'center',
   },
