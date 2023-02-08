bca1216a65a2f837a76d85da8017e3f9b089a5b0
J-Vieux <94016719+J-Vieux@users.noreply.github.com>
2023-02-08 02:39:31 -0600

    added icons

--- mello/mello/Home.js
+++ mello/mello/Home.js
@@ -4,6 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import 'react-native-gesture-handler'
+import { AntDesign } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import { Fontisto } from '@expo/vector-icons';
 
 import AgendaView from './AgendaView';
 import Settings from './Settings';
@@ -21,12 +24,13 @@ function TheTabs(){
          tabBarInactiveTintColor: '#fcf7ff', 
          tabBarActiveBackgroundColor: '#92828d',
          tabBarInactiveBackgroundColor:'#022b3a',
-         headerShown: false
+         headerShown: false,
+         showIcon: true
          }}>
-      <TabNav.Screen name='Home' component={Character}/>
-      <TabNav.Screen name="Manage Time" component={AgendaView}/>
-      <TabNav.Screen name="Groceries and Diet" component={GroceryAndDiet}/>
-      <TabNav.Screen name='Settings' component={Settings}/>
+      <TabNav.Screen name='Home' component={Character} options={{ tabBarIcon:(tabInfo) => (<MaterialCommunityIcons name="home-account" size={24} color="black" />)}}/>
+      <TabNav.Screen name="Manage Time" component={AgendaView} options={{ tabBarIcon:(tabInfo) => (<AntDesign name="book" size={24} color="black" />)}}/>
+      <TabNav.Screen name="Groceries and Diet" component={GroceryAndDiet} options={{ tabBarIcon:(tabInfo) => (<MaterialCommunityIcons name="food-variant" size={24} color="black" />)}}/>
+      <TabNav.Screen name='Settings' component={Settings} options={{ tabBarIcon:(tabInfo) => (<Fontisto name="player-settings" size={24} color="black" />)}}/>
     </TabNav.Navigator>
   );
 }
