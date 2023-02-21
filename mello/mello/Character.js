1b57b30f0a86f34d2d369f0e8fe13cf1b4d5aace
Reid Trisler <reidtrisler@yahoo.com>
2023-02-21 16:40:45 -0600

    Started Character Implementation
    Added in needed assets for first character and started implementing character screen

--- mello/mello/Character.js
+++ mello/mello/Character.js
@@ -2,14 +2,22 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import 'react-native-gesture-handler'
+import 'react-native-gesture-handler';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const BGColor = "#003847"
 
 export default function Character() {
   return (
-    <View style={styles.container}>
-      <Text>Character Screen!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <LinearGradient
+          // Background Linear Gradient
+          colors={[ BGColor, 'white']}
+          style={styles.container}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 1, y: 1 }}
+        >
+          
+    </LinearGradient>
   );
 }
 
