0f187a9fc8bb1fbfe18da68e21e1e2fd14ff8d3d
Reid Trisler <reidtrisler@yahoo.com>
2023-02-23 00:47:43 -0600

    Updating Character Screens
    Adding icons to access sub menus for character customization

--- mello/mello/Character.js
+++ mello/mello/Character.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import 'react-native-gesture-handler';
 import { LinearGradient } from 'expo-linear-gradient';
-import { Surface } from 'react-native-paper';
-import { ImageBackground } from 'react-native'
+import { Button, Surface } from 'react-native-paper';
+import { ImageBackground } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 const BGColor = "#003847"
 const LGreen = "#2AA198"
@@ -16,6 +17,9 @@ const r1body = require('./assets/img/body/robot1bodybitmap.png')
 const r1wheels = require('./assets/img/wheels/robot1wheelsbitmap.png')
 
 export default function Character() {
+
+  const [shouldShow, setShouldShow] = useState(false);
+
   return (
     <LinearGradient
           // Background Linear Gradient
@@ -24,15 +28,23 @@ export default function Character() {
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 1 }}
         >
-          <View style={styles.leftcontainer}>
-          </View>
-          <View style={styles.middlecontainer}>
-            <ImageBackground source={r1head} style={styles.head}></ImageBackground>  
-            <ImageBackground source={r1body} style={styles.body}></ImageBackground>  
-            <ImageBackground source={r1wheels} style={styles.body}></ImageBackground>  
-          </View>
-          <View style={styles.rightcontainer}>
-            <Surface style={styles.objective}></Surface>
+          <ImageBackground source={r1head} style={styles.head}></ImageBackground>  
+          <ImageBackground source={r1body} style={styles.body}></ImageBackground>  
+          <ImageBackground source={r1wheels} style={styles.body}></ImageBackground>
+          <View style={styles.iconcontainer}>
+            <Surface style={styles.iconbackground}>
+              <FontAwesome5 name="clipboard-list" size={50} color="white" />
+            </Surface>
+            <Surface style={styles.iconbackground}>
+              <FontAwesome5 name="shopping-cart" size={50} color="white" />
+            </Surface>
+            <Button style={styles.iconbackground} onPress={() => setShouldShow(!shouldShow)}>
+              <FontAwesome5 name="robot" size={50} color="white"  />
+              {shouldShow ?
+              (
+                <Surface style={styles.objective}></Surface>
+              ) : null}
+            </Button>
           </View>
     </LinearGradient>
   );
@@ -42,26 +54,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-    flexDirection: 'row',
+    flexDirection: 'column',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-  leftcontainer: {
-    flex: 1,
-    width: '30%',
-    height: '100%',
-    justifyContent: 'center',
-  },
-  middlecontainer: {
-    flex: 1,
-    width: '30%',
-    height: '100%',
-    justifyContent: 'center',
-  },
-  rightcontainer: {
-    flex: 1,
-    width: '30%',
-    height: '100%',
     justifyContent: 'center',
   },
   objective: {
@@ -76,6 +70,24 @@ const styles = StyleSheet.create({
     paddingTop: '5px',
     justifyContent: 'space-between',
   },
+  iconcontainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    marginLeft: '550px',
+    marginBottom: '50px',
+    rowGap: '25px',
+  },
+  iconbackground: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    width: '75px',
+    height: '75px',
+    backgroundColor: DGreen,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   head: {
     position: 'absolute',
     width: '430px',
@@ -89,12 +101,14 @@ const styles = StyleSheet.create({
     height: '608px',
     alignItems: 'center',
     justifyContent: 'center',
+    zIndex: 1
   },
-  body: {
+  wheels: {
     position: 'absolute',
     width: '430px',
     height: '608px',
     alignItems: 'center',
     justifyContent: 'center',
+    zIndex: 10
   },
 });
