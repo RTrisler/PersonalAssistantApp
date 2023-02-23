a2c7d3c50d05dc0efe149f057e3dd595721854f1
J-Vieux <94016719+J-Vieux@users.noreply.github.com>
2023-02-23 08:31:39 -0600

    progress bar with level indicator

--- mello/mello/Character.js
+++ mello/mello/Character.js
@@ -2,36 +2,40 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import LinearProgress from '@mui/material/LinearProgress';
 import { StyleSheet, Text, View } from 'react-native';
+import { createTheme } from '@mui/material/styles';
+import { LinearGradient } from 'expo-linear-gradient';
 
+const BGColor = "#004052"
 export default function LinearDeterminate() {
-  const [progress, setProgress] = React.useState(0);
 
-  React.useEffect(() => {
-    const timer = setInterval(() => {
-      setProgress((oldProgress) => {
-        if (oldProgress === 100) {
-          return 0;
-        }
-        const diff = Math.random() * 10;
-        return Math.min(oldProgress + diff, 100);
-      });
-    }, 500);
+const [progress, setProgress] = React.useState(0);
+const [level, setLevel] = React.useState(0);
 
-    return () => {
-      clearInterval(timer);
-    };
-  }, []);
 
   return (
     <div className='app'>
         <div className='div'>
-          <text>fart</text>
-        <LinearProgress variant="determinate" value={progress} />
+          
+          <LinearProgress variant="determinate" value={progress} color='success' 
+          sx={{
+            width: 300,
+          }}/>
+
           <button 
-            onClick={()=>
-            this.updateProgress('percent', this.state.percent + .1)
-          }>add</button>
+            onClick={()=>{ 
+              setProgress((oldProgress) => {
+                if (oldProgress === 100) {
+                  setLevel(level+1)
+                  return 0;
+                }
+                  return oldProgress + 10
+                  
+              });
+          }}>add</button> <text>Your level is: {level}</text>
+        
         </div>
       </div>
+      
   );
 }
+
