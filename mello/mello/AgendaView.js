2017192f5975535061df467912b1d52685904b2a
Reid Trisler <reidtrisler@yahoo.com>
2023-02-25 19:29:13 -0600

    Revert "Added Notification System back"
    This reverts commit 0bfb7e6a6f1cd345e41597563be60f9188438093.

--- mello/mello/AgendaView.js
+++ mello/mello/AgendaView.js
@@ -45,28 +45,6 @@ const getFormattedTime = (date) => {
 };
 export default function AgendaView() {
 
-  const [pushToken, setPushToken] = useState('');
-  const [notify, setNotify] = useState(false);
-  const notificationListener = useRef();
-  const responseListener = useRef();
-  //Notification registering stuff
-  useEffect(() => {
-    notifyRegister().then(token => setPushToken(token)).catch((error)=>console.error(error));
-    
-    notificationListener.current = Notifications.addNotificationReceivedListener(notify => {
-        setNotify(notify);
-    });
-    
-    responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
-        console.log(response);
-    });
-
-    return () => {
-        Notifications.removeNotificationSubscription(notificationListener.current);
-        Notifications.removeNotificationSubscription(responseListener.current);
-    }
-}, []);
-
   const [items, setItems] = useState({
     '2023-02-18': [
       {name: 'nametest', timeDueStart: '12:30', timeDueEnd: '13:30', note: 'note test'},
@@ -371,56 +349,3 @@ const styles = StyleSheet.create({
   }
 });
 
-//Notification system
-Notifications.setNotificationHandler({
-  handleNotification: async () => ({
-    shouldShowAlert: true,
-    shouldPlaySound: false,
-    shouldSetBadge: false,
-  }),
-});
-
-async function notifyRegister() {
-  let token;
-
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
-      importance: Notifications.AndroidImportance.MAX,
-      vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
-    });
-  }
-
-  if (Device.isDevice) {
-    const { status: existingStatus } = await Notifications.getPermissionsAsync();
-    let finalStatus = existingStatus;
-    if (existingStatus !== 'granted') {
-      const { status } = await Notifications.requestPermissionsAsync();
-      finalStatus = status;
-    }
-    if (finalStatus !== 'granted') {
-      alert('Failed to get push token for push notification!');
-      return;
-    }
-    token = (await Notifications.getExpoPushTokenAsync()).data;
-    console.log(token);
-  } else {
-    alert('Must use physical device for Push Notifications');
-  }
-
-  return token;
-}
-
-async function schedulePushNotification(message,time,triggerTime) {
-  console.log(triggerTime);
-
-  await Notifications.scheduleNotificationAsync({
-    content: {
-      title: message,
-      body: time,
-      data: { data:'goes here' }
-    },
-    trigger:{ seconds: (triggerTime/1000) - 6 },
-  });
-}
