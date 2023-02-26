2e42d513eb5267f6424379e186a064650bc8598f
Reid Trisler <reidtrisler@yahoo.com>
2023-02-25 19:25:34 -0600

    Updated Character Screen Design
    Added options to view objectives, shop, and edit appearance

--- mello/mello/Character.js
+++ mello/mello/Character.js
@@ -18,7 +18,9 @@ const r1wheels = require('./assets/img/wheels/robot1wheelsbitmap.png')
 
 export default function Character() {
 
-  const [shouldShow, setShouldShow] = useState(false);
+  const [shouldShowObjectives, setShouldShowObjectives] = useState(false);
+  const [shouldShowShop, setShouldShowShop] = useState(false);
+  const [shouldShowEditBot, setShouldShowEditBot] = useState(false);
 
   return (
     <LinearGradient
@@ -28,23 +30,41 @@ export default function Character() {
           start={{ x: 0, y: 0 }}
           end={{ x: 1, y: 1 }}
         >
-          <ImageBackground source={r1head} style={styles.head}></ImageBackground>  
-          <ImageBackground source={r1body} style={styles.body}></ImageBackground>  
-          <ImageBackground source={r1wheels} style={styles.body}></ImageBackground>
+          <View style={styles.charactercontainer}>
+            <ImageBackground source={r1head} style={styles.head}></ImageBackground>
+            <ImageBackground source={r1body} style={styles.body}></ImageBackground>  
+            <ImageBackground source={r1wheels} style={styles.wheels}></ImageBackground>
+          </View>
           <View style={styles.iconcontainer}>
-            <Surface style={styles.iconbackground}>
+            <Button style={styles.iconbackground}  onPress={() => {setShouldShowObjectives(!shouldShowObjectives); setShouldShowShop(false); setShouldShowEditBot(false)}}>
               <FontAwesome5 name="clipboard-list" size={50} color="white" />
-            </Surface>
-            <Surface style={styles.iconbackground}>
-              <FontAwesome5 name="shopping-cart" size={50} color="white" />
-            </Surface>
-            <Button style={styles.iconbackground} onPress={() => setShouldShow(!shouldShow)}>
-              <FontAwesome5 name="robot" size={50} color="white"  />
-              {shouldShow ?
-              (
-                <Surface style={styles.objective}></Surface>
-              ) : null}
             </Button>
+            <Button style={styles.iconbackground}  onPress={() => {setShouldShowShop(!shouldShowShop); setShouldShowObjectives(false); setShouldShowEditBot(false)}}>
+              <FontAwesome5 name="shopping-cart" size={50} color="white" />
+            </Button>
+            <Button style={styles.iconbackground} onPress={() => {setShouldShowEditBot(!shouldShowEditBot); setShouldShowShop(false); setShouldShowObjectives(false)}}>
+              <FontAwesome5 name="robot" size={50} color="white" style={styles.icon} />
+            </Button>
+          </View>
+          <View style={styles.popupcontainer}>
+            {shouldShowObjectives ?
+              (
+                <Surface style={styles.objectives}>
+                  <Text>Objectives</Text>
+                </Surface>
+              ) : null}
+              {shouldShowShop ?
+              (
+                <Surface style={styles.shop}>
+                  <Text>Shop</Text>
+                </Surface>
+              ) : null}
+              {shouldShowEditBot ?
+              (
+                <Surface style={styles.editbot}>
+                  <Text>Edit Appearance</Text>
+                </Surface>
+              ) : null}
           </View>
     </LinearGradient>
   );
@@ -53,62 +73,97 @@ export default function Character() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    rowGap: '20px',
   },
-  objective: {
+  charactercontainer: {
     display: 'flex',
+    width: '480px',
     flexDirection: 'column',
-    flexWrap: 'wrap',
-    width: '80%',
-    height: '40%',
-    backgroundColor: DGreen,
-    borderRadius: '10px',
-    marginLeft: '50px',
-    paddingTop: '5px',
-    justifyContent: 'space-between',
+    alignSelf: 'center',
+  },
+  head: {
+    width: '430px',
+    height: '608px',
+    marginBottom: '-608px',
+  },
+  body: {
+    width: '430px',
+    height: '608px',
+    zIndex: 1
+  },
+  wheels: {
+    width: '430px',
+    height: '608px',
+    marginTop: '-608px',
+    zIndex: 0
   },
   iconcontainer: {
     display: 'flex',
     flexDirection: 'column',
     flexWrap: 'wrap',
-    marginLeft: '550px',
-    marginBottom: '50px',
-    rowGap: '25px',
+    width: '110px',
+    rowGap: '50px',
+    marginBottom:'100px',
+    alignSelf: 'center',
   },
   iconbackground: {
     display: 'flex',
-    flexDirection: 'column',
-    flexWrap: 'wrap',
-    width: '75px',
-    height: '75px',
+    width: '100px',
+    height: '100px',
     backgroundColor: DGreen,
     justifyContent: 'center',
+  },
+  icon: {
+    flex: 5,
+    alignSelf: 'center',
+    justifyContent: 'center',
     alignItems: 'center',
   },
-  head: {
-    position: 'absolute',
-    width: '430px',
-    height: '608px',
+  popupcontainer: {
+    display: 'flex',
+    width: '480px',
+    height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  body: {
-    position: 'absolute',
-    width: '430px',
-    height: '608px',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 1
+  objectives: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    width: '100%',
+    height: '400px',
+    backgroundColor: DGreen,
+    borderRadius: '10px',
+    marginBottom:'100px',
+    marginLeft: '50px',
+    paddingTop: '5px',
+    justifyContent: 'space-between',
   },
-  wheels: {
-    position: 'absolute',
-    width: '430px',
-    height: '608px',
-    alignItems: 'center',
-    justifyContent: 'center',
-    zIndex: 10
+  shop: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    width: '100%',
+    height: '400px',
+    backgroundColor: DGreen,
+    borderRadius: '10px',
+    marginBottom:'100px',
+    marginLeft: '50px',
+    paddingTop: '5px',
+    justifyContent: 'space-between',
+  },
+  editbot: {
+    display: 'flex',
+    flexDirection: 'column',
+    flexWrap: 'wrap',
+    width: '100%',
+    height: '400px',
+    backgroundColor: DGreen,
+    borderRadius: '10px',
+    marginBottom:'100px',
+    marginLeft: '50px',
+    paddingTop: '5px',
+    justifyContent: 'space-between',
   },
 });
