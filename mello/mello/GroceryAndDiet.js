90deb03eebe6eacd399062f5089963afdc2a400f
Thien Vu <1234thien@gmail.com>
2023-02-25 19:37:30 -0600

    added recipe adder

--- mello/mello/GroceryAndDiet.js
+++ mello/mello/GroceryAndDiet.js
@@ -4,6 +4,7 @@ import { TextInput, List, Checkbox, Divider, Surface, Text, Button, IconButton, 
 import axios from 'axios';
 import { SelectList } from 'react-native-dropdown-select-list';
 import Toast from 'react-native-toast-message';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const BGColor = "#003847"
 const LGreen = "#2AA198"
@@ -28,6 +29,7 @@ export default function GroceryAndDiet() {
   const hideItemAdder = () => setItemAdderVisible(false);
   const [list, setList] = useState([]);
   const [newItemName, setNewItemName] = useState('');
+  const [newItemQty, setNewItemQty] = useState(1);
   //the actual adding part
   const handleAddItem = async () => {
     if(newItemName == '') {
@@ -38,13 +40,15 @@ export default function GroceryAndDiet() {
       return;
     }
     const itemToAdd = newItemName;
+    const itemQty = newItemQty ? newItemQty : 1;
     //reset newitemname
     setNewItemName('');
+    setNewItemQty(1);
     const oldList = list;
     const cat = category || -1;
     console.log(cat);
     //add to list quickly
-    setList([...oldList, { name: itemToAdd, ID: -1,  foodData: "" , itemCategory: cat == -1 ? "" : categories[cat]}]);
+    setList([...oldList, { name: itemToAdd, ID: -1,  foodData: "" , itemCategory: cat == -1 ? "" : categories[cat], Qty: itemQty}]);
     hideItemAdder();
     //if not a food then no point in getting data from database
     if(category == '' || cat > 7) return;
@@ -70,8 +74,8 @@ export default function GroceryAndDiet() {
         const protV = protein ? (protein.amount + protein.unitName) : "0g";
         const fatV = fat ? (fat.amount + fat.unitName) : "0g";
         const cholV = cholesterol ? (cholesterol.amount + cholesterol.unitName) : "0g";
-        let fDataStr = "Cal " + calV + "\t " + "Carbs " + carbV + "\t " + "Protein " + protV  + "\n" + "Fat " + fatV + "\t " + "Cholesterol " + cholV;
-        setList([...oldList, { name: itemToAdd, ID: fData[0],  foodData: fDataStr, itemCategory: cat == -1 ? "" : categories[cat] }]);
+        let fDataStr = "Cal " + calV + "\t" + "Carbs " + carbV + "\t" + "Protein " + protV  + "\n" + "Fat " + fatV + "\t" + "Cholesterol " + cholV;
+        setList([...oldList, { name: itemToAdd, ID: fData[0],  foodData: fDataStr, itemCategory: cat == -1 ? "" : categories[cat], Qty: itemQty }]);
     }
     
 
@@ -103,58 +107,131 @@ export default function GroceryAndDiet() {
   const toggleGroceryView = () => {
     setGroceryView(!groceryView);
   };
+
+
+  const [recipeAdderVisible, setRecipeAdderVisible] = useState(false);
+  const hideRecipeAdder = () => {
+    setRecipeAdderVisible(false);
+  };
+  const showRecipeAdder = () => {
+    setRecipeAdderVisible(true);
+  };
+  const [newRecipeName, setNewRecipeName] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const handleAddRecipe = () => {
+    setRecipes([...recipes, {name: newRecipeName}]);
+  };
+
+
   return (
     <SafeAreaView style={styles.container}>
       {groceryView ? (
-      <View>
-      <View style={{flexDirection: 'row', paddingHorizontal: '1%', justifyContent: 'space-between'}}>
-        <Button onPress={showItemAdder} style={{...styles.showAdderButton}}><Text style={{fontSize: 20, fontWeight: 'bold', color: LGreen}}>Add Item</Text></Button>
-        <IconButton icon='book' iconColor={LGreen} onPress={toggleGroceryView}></IconButton>
-      </View>
-      <List.Section style={{backgroundColor: BGColor}}>
-        {list.map((item, index) => (
-          <Surface key={item.name} style={styles.itemContainer}>
-            <View style={styles.itemDetailsContainer}>
-              <View style ={{justifyContent: 'space-between', flexDirection: 'row'}}>
-                <Text style={styles.itemText}>{item.name}</Text>
-                <Text style={styles.categoryText}>{item.itemCategory.value}</Text>
+      <View style={{flex:1}}>
+        <View style={{flexDirection: 'row', paddingHorizontal: '1%', justifyContent: 'space-between'}}>
+          <Button onPress={showItemAdder} style={{...styles.showAdderButton}}><Text style={{fontSize: 20, fontWeight: 'bold', color: LGreen}}>Add Grocery Item</Text></Button>
+          <IconButton icon='book' iconColor={LGreen} onPress={toggleGroceryView}></IconButton>
+        </View>
+        <ScrollView>
+        <List.Section style={{backgroundColor: BGColor}}>
+          {list.map((item, index) => (
+            <Surface key={item.name} style={styles.itemContainer}>
+              <View style={{flexDirection: 'row'}}>
+                <View style={styles.itemDetailsContainer}>
+                  <View style ={{justifyContent: 'space-between', flexDirection: 'row'}}>
+                    <Text style={styles.itemText}>{item.name}</Text>
+                    <Text style={styles.categoryText}>{item.itemCategory.value}</Text>
+                  </View>
+                </View>
+                <View style={{backgroundColor: DGreen, minWidth: '10%', justifyContent: 'center', alignItems: 'center'}}>
+                  <Text style={{color: LGreen, fontSize: 25, fontWeight: 'bold'}}>{item.Qty}</Text>
+                </View>
+                <IconButton
+                  onPress={() => handleDeleteItem(index)}
+                  icon="delete"
+                  iconColor={LGreen}
+                >
+                </IconButton>
               </View>
-              <Text style={styles.itemDetails}>{item.foodData}</Text>
-            </View>
-            <IconButton
-              onPress={() => handleDeleteItem(index)}
-              icon="delete"
-              iconColor={LGreen}
-            >
-            </IconButton>
-          </Surface>
-        ))}
-      </List.Section>
-
-        </View>)
+              {(item.ID != -1) && <List.Accordion style={{minWidth: '100%', backgroundColor: DGreen, maxHeight: '3%'}}>
+                <Text style={styles.itemDetails}>{item.foodData}</Text>
+              </List.Accordion>}
+            </Surface>
+          ))}
+        </List.Section>
+        </ScrollView>
+        </View>
+        )
         :
         (
-          <View>
-
+        <View style={{flex:1}}>
+          <View style={{flexDirection: 'row', paddingHorizontal: '1%', justifyContent: 'space-between'}}>
+            <Button onPress={showRecipeAdder} style={{...styles.showAdderButton}}><Text style={{fontSize: 20, fontWeight: 'bold', color: LGreen}}>Add Recipe</Text></Button>
+            <IconButton icon='book' iconColor={LGreen} onPress={toggleGroceryView}></IconButton>
           </View>
-        )}
-              <Modal visible={itemAdderVisible} onDismiss={hideItemAdder}>
-            <Card style={styles.itemAdder}>
-              <TextInput placeholder='Item name' onChangeText={setNewItemName} textColor="#2AA198" value={newItemName} style={{minWidth: '80%'}}/>
-              <SelectList setSelected={setCategory} data={categories} save="key" inputStyles={{color: LGreen}} dropdownTextStyles={{color: LGreen}}/>
-              <View style={{flexDirection:'row', justifyContent:'space-between', paddingVertical: 10, paddingHorizontal: 5}}>
-              <Button onPress={handleAddItem} buttonColor={DGreen} textColor={LGreen}>
-                <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Save </Text>
-              </Button>
-              <Button onPress={hideItemAdder} buttonColor={DGreen} textColor={LGreen}>
-                <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Cancel </Text>
-              </Button>
+          <ScrollView>
+        <List.Section style={{backgroundColor: BGColor}}>
+          {recipes.map((item, index) => (
+            <Surface key={item.name} style={styles.itemContainer}>
+              <View style={styles.itemDetailsContainer}>
+                <View style ={{justifyContent: 'space-between', flexDirection: 'row'}}>
+                  <Text style={styles.itemText}>{item.name}</Text>
+                  <Text style={styles.categoryText}>{item.name}</Text>
+                </View>
+                <Text style={styles.itemDetails}>{item.name}</Text>
               </View>
-            </Card>
-            <Toast />
+              <View style={{backgroundColor: DGreen, minWidth: '10%', justifyContent: 'center', alignItems: 'center'}}>
+                <Text style={{color: LGreen, fontSize: 25, fontWeight: 'bold'}}>{item.name}</Text>
+              </View>
+              <IconButton
+                onPress={() => handleDeleteItem(index)}
+                icon="delete"
+                iconColor={LGreen}
+              >
+              </IconButton>
+            </Surface>
+          ))}
+        </List.Section>
+        </ScrollView>
+        </View>
+        )}
+
+
+        
+        <Modal visible={itemAdderVisible} onDismiss={hideItemAdder}>
+          <Card style={styles.itemAdder}>
+            <View style={{flexDirection: 'row'}}>
+              <TextInput placeholder='Item name' onChangeText={setNewItemName} textColor="#2AA198" value={newItemName} style={{minWidth: '75%', backgroundColor: BGColor}}/>
+              <TextInput placeholder='Qty.' onChangeText={setNewItemQty} textColor="#2AA198" value={newItemQty} style={{minWidth: '25%', backgroundColor: DGreen}} keyboardType='numeric'/>
+            </View>
+            <SelectList setSelected={setCategory} data={categories} save="key" inputStyles={{color: LGreen}} dropdownTextStyles={{color: LGreen}}/>
+            <View style={{flexDirection:'row', justifyContent:'space-between', paddingVertical: 10, paddingHorizontal: 5}}>
+            <Button onPress={handleAddItem} buttonColor={DGreen} textColor={LGreen}>
+              <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Save </Text>
+            </Button>
+            <Button onPress={hideItemAdder} buttonColor={DGreen} textColor={LGreen}>
+              <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Cancel </Text>
+            </Button>
+            </View>
+          </Card>
         </Modal>
+
+
+        <Modal visible={recipeAdderVisible} onDismiss={hideRecipeAdder}>
+          <Card style={styles.itemAdder}>
+            <TextInput placeholder='Recipe name' onChangeText={setNewRecipeName} textColor="#2AA198" value={newRecipeName} style={{minWidth: '80%'}}/>
+            <View style={{flexDirection:'row', justifyContent:'space-between', paddingVertical: 10, paddingHorizontal: 5}}>
+            <Button onPress={handleAddRecipe} buttonColor={DGreen} textColor={LGreen}>
+              <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Save </Text>
+            </Button>
+            <Button onPress={hideRecipeAdder} buttonColor={DGreen} textColor={LGreen}>
+              <Text style = {{fontSize: 20, fontWeight: 'bold', color: LGreen}}> Cancel </Text>
+            </Button>
+            </View>
+          </Card>
+        </Modal>
+
+        <Toast />
     </SafeAreaView>
-    
   );
 };
 
@@ -179,11 +256,10 @@ const styles = StyleSheet.create({
   },
   itemContainer: {
     backgroundColor: BGColor,
-    flexDirection: 'row',
     alignItems: 'center',
   },
   itemDetailsContainer: {
-    marginLeft: 20,
+    paddingLeft: 10,
     flex: 1,
   },
   itemText: {
@@ -195,6 +271,7 @@ const styles = StyleSheet.create({
     color: LGreen
   },
   itemDetails: {
+    paddingLeft: 10,
     fontSize: 15,
     color: LGreen
   },
