aae1a3815b2f0029d877dd1ee2d48d98c8c68248
Reid Trisler <reidtrisler@yahoo.com>
2023-03-23 15:03:10 -0500

    Redesigned Login Screen

--- mello/mello/screens/LoginScreen.js
+++ mello/mello/screens/LoginScreen.js
@@ -1,11 +1,23 @@
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
+import { KeyboardAvoidingView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View, Image,  Dimensions } from 'react-native'
 import React, { useState, useEffect } from 'react'
 import { auth } from "../firebase"
 import { useNavigation } from '@react-navigation/native'
+import { BlurView } from 'expo-blur';
+import { ImageBackground } from 'react-native-web';
+import { useFonts } from 'expo-font';
+
+const wave = require('/assets/img/wave/wave.png')
+const r1head = require('/assets/img/head/robot1headbitmap.png')
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+const BGColor = "#003847";
 
 const LoginScreen = () => {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
+    const [fontsLoaded] = useFonts({
+        'Elnath': require('/assets/fonts/ELNATH.ttf'),
+    });
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
 
     const navigation = useNavigation()
 
@@ -40,42 +52,55 @@ const LoginScreen = () => {
     }
 
   return (
-    <KeyboardAvoidingView
+    <View
         style={styles.container}
-        behavior="padding"
     >
-        <View style={styles.inputContainer}>
-            <TextInput
-                placeholder="Email"
-                value={email}
-                onChangeText={text => setEmail(text)}
-                style={styles.input}
-            />
-            <TextInput
-                placeholder="Password"
-                value={password}
-                onChangeText={text => setPassword(text)}
-                style={styles.input}
-                secureTextEntry
-            />
-        </View>
+        <ImageBackground source={wave} style={[styles.wave, StyleSheet.absoluteFill]} />
+            <ScrollView contentContainerStyle={{
+                flex: 1,
+                width: '100%',
+                height: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}>
+            <BlurView intensity={100}>
+                <View style={styles.login}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.title}>Mello</Text>
+                    </View>
+                    <View>
+                        <TextInput
+                            placeholder="Email"
+                            value={email}
+                            onChangeText={text => setEmail(text)}
+                            style={styles.input}
+                        />
+                        <TextInput
+                            placeholder="Password"
+                            value={password}
+                            onChangeText={text => setPassword(text)}
+                            style={styles.input}
+                            secureTextEntry
+                        />
+                    </View>
+                    <TouchableOpacity
+                        onPress={handleLogin} 
+                        style={styles.button}
+                    >
+                        <Text style={styles.buttonText}>Login</Text>
+                    </TouchableOpacity>
 
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity
-                onPress={handleLogin} 
-                style={styles.button}
-            >
-                <Text style={styles.buttonText}>Login</Text>
-            </TouchableOpacity>
-
-            <TouchableOpacity
-                onPress={handleSignUp} 
-                style={[styles.button, styles.buttonOutline]}
-            >
-                <Text style={styles.buttonOutlineText}>Register</Text>
-            </TouchableOpacity>
-        </View>
-    </KeyboardAvoidingView>
+                    <TouchableOpacity
+                        onPress={handleSignUp} 
+                        style={styles.button}
+                    >
+                        <Text style={styles.buttonText}>Register</Text>
+                    </TouchableOpacity>
+                    
+                </View>
+            </BlurView>
+        </ScrollView>
+    </View>
   )
 }
 
@@ -87,15 +112,51 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    wave: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+    },
+    login: {
+        width: 650,
+        height: 550,
+        borderColor: '#fff',
+        borderWidth: 2,
+        borderRadius: 10,
+        padding: 10,
+        alignItems: 'center',
+    },
     inputContainer: {
-        width: '80%'
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',   
+    },
+    titleContainer: {
+        width: 200,
+        height: 100,
+        marginVertical: 30,
+        justifyContent: 'center',
+    },
+    title: {
+        fontFamily: 'Elnath',
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        fontSize: 100,
+        color: '#fff',
     },
     input: {
         backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
+        width: 450,
+        height: 40,
         borderRadius: 10,
-        marginTop: 5,
+        borderColor: '#fff',
+        borderWidth: 2,
+        padding: 10,
+        marginVertical: 10,
+        backgroundColor: '#ffffff90',
+        marginBottom: 20
     },
     buttonContainer: {
         width: '60%',
@@ -104,11 +165,15 @@ const styles = StyleSheet.create({
         marginTop: 40,
     },
     button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
+        backgroundColor: BGColor,
+        width: 450,
+        height: 40,
         borderRadius: 10,
+        borderColor: '#fff',
+        borderWidth: 2,
         alignItems: 'center',
+        justifyContent: 'center',
+        marginVertical: 10,
     },
     buttonText: {
         color: 'white',
@@ -118,11 +183,11 @@ const styles = StyleSheet.create({
     buttonOutline: {
         backgroundColor: 'white',
         marginTop: 5,
-        borderColor: '#0782F9',
+        borderColor: '#fff',
         borderWidth: 2,
     },
     buttonOutlineText: {
-        color: '#0782F9',
+        color: '#fff',
         fontWeight: '700',
         fontSize: 16,
     },
