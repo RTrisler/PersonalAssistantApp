8b2ff26b9dcc73f921cce03ca5c43f3abb7e4e17
J-Vieux <94016719+J-Vieux@users.noreply.github.com>
2023-04-11 09:05:58 -0500

    char update

--- mello/mello/Character.js
+++ mello/mello/Character.js
@@ -20,6 +20,7 @@ const DGreen = "#002B36"
 const r1head = require('./assets/img/head/robot1headbitmap.png')
 const r1body = require('./assets/img/body/robot1bodybitmap.png')
 const r1wheels = require('./assets/img/wheels/robot1wheelsbitmap.png')
+const robo = require('./assets/img/robot/robot2.png')
 
 const incrementingValues = [1,3,3,3];
 const valuesNeeded = [1,0,0,0];
@@ -93,9 +94,7 @@ export default function Character() {
     <View style={styles.container}>
       <View style={styles.charactercontainer}>
         <View style={{justifyContent: 'center', alignItems: 'center'}}>
-        <ImageBackground source={r1head} style={styles.head}></ImageBackground>
-        <ImageBackground source={r1body} style={styles.body}></ImageBackground>  
-        <ImageBackground source={r1wheels} style={styles.wheels}></ImageBackground>
+        <ImageBackground source={robo} style={styles.wheels}></ImageBackground>
         </View>
         <View className='div'>
           <ProgressBar progress={progress/100} style={{borderRadius: 20, height: 20}} color={LGreen}/>
@@ -213,7 +212,7 @@ const styles = StyleSheet.create({
   wheels: {
     width: '215px',
     height: '304px',
-    marginTop: '-304px',
+    marginTop: '-75px',
     zIndex: 0
   },
   iconcontainer: {
