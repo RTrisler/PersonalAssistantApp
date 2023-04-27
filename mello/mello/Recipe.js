685da21dcb5f9d4ef0016f6b78a49a8652efbf8b
Thien Vu <1234thien@gmail.com>
2023-04-27 08:24:53 -0500

    recipes

--- mello/mello/Recipe.js
+++ mello/mello/Recipe.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react"
 import { StyleSheet, Text, View, TouchableOpacity, Image, Button, ScrollView} from 'react-native';
 import { Surface} from "react-native-paper"
-
+import { getDatabase, ref, get, set, onValue } from 'firebase/database';
 
 const BGColor = "#003847";
 
@@ -27,6 +27,22 @@ export default function Recipe({ meal }) {
       })
   }, [meal.id])
 
+  const db = getDatabase();
+
+  const [pantry, setPantry] = useState([]);
+  const dbPantry = ref(db, 'users/userID/pantry')
+  useEffect(() => {
+    get(dbPantry).then((snapshot) => {
+      if(snapshot.exists()) {
+        setPantry(snapshot.val());
+      }
+      else {
+        set(dbPantry, pantry);
+      }
+    }, []);
+  }, []);
+
+
   return (
     <View style={{ flex: 1, margin: '10px', }}>
             <View style={{ backgroundColor: "#eee", borderRadius: 10, overflow: "hidden", width: '600px' }}>
@@ -46,7 +62,12 @@ export default function Recipe({ meal }) {
                 </Text>
                 <ScrollView>
                     {recipeData && recipeData.extendedIngredients.map(ingredent => {
-                        return <Text key={ingredent.id}>{ingredent.name}</Text>;
+                        return <Text 
+                                key={ingredent.id} 
+                                style={{color: (pantry.map(item => item.name.toLowerCase()).includes(ingredent.name.toLowerCase())) ? "#22EE22" : "#EE2222"}}
+                                >
+                                  {ingredent.name}
+                                </Text>;
                         }) }
                 </ScrollView>
                     
@@ -62,6 +83,32 @@ export default function Recipe({ meal }) {
                     <Button
                         title='Add To My Recipes'
                         color='#003847'
+                        onPress={() => {
+                          if (!recipeData || !recipeData.extendedIngredients || !recipeData.analyzedInstructions) { return; }
+
+                          const db = getDatabase();
+                          const dbRecipes = ref(db, 'users/userID/recipes')
+                          console.log(recipeData);
+                          console.log(recipeData.analyzedInstructions)
+                          console.log(recipeData.analyzedInstructions[0])
+                          const newRecipe = {
+                            name: meal.title, 
+                            ingredients: (recipeData.extendedIngredients != [] ? recipeData.extendedIngredients.map(ing => {return {name: ing.name, Qty: ing.amount}}) : []),
+                            steps: (((recipeData.analyzedInstructions != []) && recipeData.analyzedInstructions[0]) ? recipeData.analyzedInstructions[0].steps.map(steps => steps.step) : [])
+                          }
+
+                          get(dbRecipes).then((snapshot) => {
+                            console.log(snapshot.exists())
+                            if(snapshot.exists()) {
+                              let recipes = snapshot.val();
+                              recipes = [...recipes, newRecipe]
+                              set(dbRecipes, recipes);
+                            }
+                            else {
+                              set(dbRecipes, [newRecipe])
+                            }
+                          });
+                        }}
                         >
 
                     </Button>
